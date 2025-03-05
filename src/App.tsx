adf2196daa0b8b8a130dde7f382186666965d33a
Roman Veselý <rvesely88@gmail.com>
2025-03-05 09:54:28 +0100

    test apply class schema from prisma - pydantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css';
 import { Header } from './components/Header';
-import { JsonFormsDemo } from './components/JsonFormsDemo';
+import { JsonFormsPrismaDemo } from './components/JsonFormsPrismaDemo';
+
+
+// import { Prisma, PrismaClient } from '@prisma/client'
+// import type * as Prisma from '@prisma/client';
+// import type { User } from '@prisma/client'
+
+// Org is a model in this example
+//const Test = Prisma.User; // works
+
+// const prisma = new PrismaClient()
+
+
+
+//console.log(prisma.user.fields)
+console.log('GetServerSideProps - finish');
 
 const App = () => {
   return (
     <>
       <Header />
-      <JsonFormsDemo />
+      <JsonFormsPrismaDemo />
     </>
   );
 };
--- vite.config.mts
+++ vite.config.mts
@@ -1,10 +1,17 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vitest/config';
+import dns from 'node:dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 export default defineConfig({
   plugins: [react()],
+
   build: {
     outDir: 'build',
+  },
+  server: {
+    cors: true,
   },
   test: {
     environment: 'jsdom',
